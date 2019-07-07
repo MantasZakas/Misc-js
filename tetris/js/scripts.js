bc845dfb612f4737dcfcc4613ae7845ea54435bb
Mantas Zakas <mantas.zakas@gmail.com>
2019-07-07 15:55:12 +0300

    Segments cannot be moved out of play field
    Also, unbound key presses are ignored

--- tetris/js/scripts.js
+++ tetris/js/scripts.js
@@ -1,8 +1,8 @@
 "use strict";
 
-$(function() {
+$(function () {
 
-    let positions = [[4, 140], [5, 140], [4, 150], [5, 150]];
+    let activePositions = [[4, 140], [5, 140], [4, 150], [5, 150]];
 
     prepareTetrisField();
     setKeyListener();
@@ -11,7 +11,7 @@ $(function() {
     /**
      * Prepare the table that serves as the play field
      */
-    function prepareTetrisField () {
+    function prepareTetrisField() {
         let tetrisField = $("#tetris_field");
         for (let rowNo = 200; rowNo > 0; rowNo -= 10) {
             let fieldRow = '<tr>';
@@ -26,16 +26,22 @@ $(function() {
     /**
      * Add keyboard listener which allows controlled movement of tetris segments
      */
-    function setKeyListener () { //TODO stop tetrominos from moving outside of field
+    function setKeyListener() { //TODO stop tetrominos from moving outside of field
         $(document).keydown(function (e) {
-            let newPositions = [];
-            if (e.which === 40) newPositions = generateNewPositions(1,-10);
-            if (e.which === 38) newPositions = generateNewPositions(1,10);
-            if (e.which === 37) newPositions = generateNewPositions(0,-1);
-            if (e.which === 39) newPositions = generateNewPositions(0,1);
-            moveTetrisSegments(false); //delete old segments
-            positions = newPositions.slice();
-            moveTetrisSegments(true); //draw new segments
+            console.log(e.which);
+            let recognisedKeys = [40, 38, 37, 39]; //TODO might need to change this to a key mapping object
+            if (recognisedKeys.includes(e.which)) { //ignore not understood key presses
+                let newPositions = [];
+                if (e.which === 40) newPositions = generateNewPositions(1, -10);//down
+                if (e.which === 38) newPositions = generateNewPositions(1, 10);//up
+                if (e.which === 37) newPositions = generateNewPositions(0, -1);//left
+                if (e.which === 39) newPositions = generateNewPositions(0, 1);//right
+                if (activePositions !== newPositions) {
+                    moveTetrisSegments(false); //delete old segments
+                    activePositions = newPositions.slice();
+                    moveTetrisSegments(true); //draw new segments
+                }
+            }
         })
     }
 
@@ -45,22 +51,37 @@ $(function() {
      * @param {number} magnitude // +-1 or +-10
      * @returns {Array}
      */
-    function generateNewPositions (colOrRow, magnitude) {
+    function generateNewPositions(colOrRow, magnitude) {
         let newPositions = [];
-        positions.forEach(function(position) {
-            let newPosition = position.slice();
+        for (let positionNo in activePositions) {
+            let newPosition = activePositions[positionNo].slice();
             newPosition[colOrRow] += magnitude;
+            if (!validateNewPosition(newPosition)) {
+                newPositions = activePositions.slice();
+                break //TODO might need to return some king of signal here
+            }
             newPositions.push(newPosition);
-        });
+        }
         return newPositions
     }
+
+    // function generateNewPositions (colOrRow, magnitude) {
+    //     let newPositions = [];
+    //     activePositions.forEach(function(position) {
+    //         let newPosition = position.slice();
+    //         newPosition[colOrRow] += magnitude;
+    //         if (!validateNewPosition(newPosition)) return activePositions;
+    //         newPositions.push(newPosition);
+    //     });
+    //     return newPositions
+    // }
 
     /**
      * Draw or delete tetris segments
      * @param {boolean} draw // true for draw; false for delete
      */
-    function moveTetrisSegments (draw) {
-        positions.forEach(function(position) {
+    function moveTetrisSegments(draw) {
+        activePositions.forEach(function (position) {
             let tetrisCell = document.getElementsByClassName(position[1].toString())[position[0]];
             if (draw) {
                 tetrisCell.innerHTML = '<div class="tetris-segment"></div>'
@@ -68,5 +89,17 @@ $(function() {
                 tetrisCell.innerHTML = ''
             }
         }); //TODO add stability by checking segments and cells before drawing or deleting
+    }
+
+    /**
+     * Checks if new position of a segment is inside the play field
+     * @param {array} newPosition
+     * @returns {boolean}
+     */
+    function validateNewPosition(newPosition) { //TODO automate tetris field size
+        let valid = true;
+        if (newPosition[0] < 0 || newPosition[0] > 9) valid = false;
+        if (newPosition[1] < 10 || newPosition[1] > 200) valid = false;
+        return valid
     }
 });
