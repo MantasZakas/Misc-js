097fe7a1d86c6541029b229a9bff2323fe50d58e
Mantas Zakas <mantas.zakas@gmail.com>
2019-07-07 10:55:32 +0300

    Tetris segment movement implemented

--- tetris/js/scripts.js
+++ tetris/js/scripts.js
@@ -2,11 +2,13 @@
 
 $(function() {
 
+    let position = [4, 140];
+
     prepareTetrisField();
     setKeyListener();
+    move(); //TODO remove this line later
 
-    let position = [4, 140];
-    test();
+
 
     /**
      * Prepare the table that serves as the play field
@@ -23,14 +25,26 @@ $(function() {
         }
     }
 
+    /**
+     * Add keyboard listener which allows controlled movement of tetris segments
+     */
     function setKeyListener () {
         $(document).keydown(function (e) {
             console.log(e.which);
-            
+            let newPosition = position.slice();
+            if (e.which === 40 && newPosition[1] > 10) newPosition[1] -= 10;
+            if (e.which === 38 && newPosition[1] < 200) newPosition[1] += 10;
+            if (e.which === 37 && newPosition[0] > 0) newPosition[0] -= 1;
+            if (e.which === 39 && newPosition[0] < 9) newPosition[0] += 1;
+            let originalTetrisCell = document.getElementsByClassName(position[1].toString())[position[0]];
+            originalTetrisCell.innerHTML = '';
+            // document.getElementsByClassName(position[1].toString())[position[0]].innerHTML = '';
+            position = newPosition.slice();
+            move();
         })
     }
 
-    function test () {
+    function move () {
         let col = position[0];
         let row = position[1];
         row = row.toString();
