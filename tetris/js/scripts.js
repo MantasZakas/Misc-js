4106d540eb5784c4be909e004c8d203d4b4a7bdb
Mantas Zakas <mantas.zakas@gmail.com>
2019-08-03 20:00:30 +0300

    Collision, turn changing and game over implemented

--- tetris/js/scripts.js
+++ tetris/js/scripts.js
@@ -6,10 +6,13 @@ $(function () {
     const TETRIS_FIELD_WIDTH = 10;
     let tetrisFieldArray = [];
     let activePositions = [[2, 4], [2, 5], [3, 4], [3, 5]];
+    let turnTimer;
 
     prepareFieldArray();
     setKeyListener();
-    updateFieldArray(1);
+    updateFieldArray(1);//display initial set up
+    test(); //TODO delete this later
+    turnTimer = setInterval(changeTurn, 1000); //TODO use clearInterval(turnTimer) on game over
 
     /**
      * Prep the array that will represent the playing field
@@ -56,10 +59,9 @@ $(function () {
         window.addEventListener("keydown", function (e) {
             let recognisedKeys = [40, 38, 37, 39]; //TODO might need to change this to a key mapping object
             if (recognisedKeys.includes(e.which)) { //ignore not understood key presses
-                console.log(e.which);
                 switch (e.which) {
                     case 40:
-                        shiftPositions(1, 0);//down
+                        shiftPositions(1, 0);//down TODO this should change turns instead of going down
                         break;
                     case 38:
                         shiftPositions(-1, 0);//up TODO remove later
@@ -103,9 +105,49 @@ $(function () {
             let newColPosition = activePositions[positionNo][1] + colShift;
             if (newRowPosition < 0 || newRowPosition >= TETRIS_FIELD_HEIGHT + 4 || newColPosition < 0 || newColPosition >= TETRIS_FIELD_WIDTH)
                 moveIsValid = false; //tetris field edge collision
-            //TODO add collision with occupied cells here
+            if (tetrisFieldArray[newRowPosition][newColPosition] === 2)
+                moveIsValid = false //taken cell collision
         }
         return moveIsValid;
+    }
+
+    /**
+     * Returns the number of taken cells in a given row
+     * @param {number}rowNo
+     * @returns {number}
+     */
+    function checkRow(rowNo) {
+        let takenCells = 0;
+        for (let cellNo in tetrisFieldArray[rowNo]) {
+            if (tetrisFieldArray[rowNo][cellNo] === 2)
+                takenCells ++;
+        }
+        console.log(takenCells);
+        return takenCells;
+    }
+
+    /**
+     * Implements turn changing
+     */
+    function changeTurn() {
+        if (validateMovement(1, 0))//if there is space underneath
+            shiftPositions(1, 0);//simulate gravity
+        else {
+            if (checkRow(3) > 0) {//if there are taken cells in the spawn area
+                clearInterval(turnTimer);
+                console.log("Game over");//TODO change this to a game over screen
+                return;
+            }
+            updateFieldArray(2);//change active tetromino to inactive cells
+            activePositions = [[2, 4], [2, 5], [3, 4], [3, 5]];//spawn a new tetromino TODO make it a random tetromino
+            changeTurn();//do not skip a turn when spawning a new tetromino
+        }
+    }
+
+    function test() {//TODO delete this later
+        tetrisFieldArray[10][4] = 2;
+        tetrisFieldArray[10][5] = 2;
+        displayFieldArray()
     }
 
 });
