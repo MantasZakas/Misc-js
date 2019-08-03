b6da3b9ac6337217065a71a2f8baa4ab4c24a2ce
Mantas Zakas <mantas.zakas@gmail.com>
2019-08-03 20:51:35 +0300

    Filled row clearing implemented

--- tetris/js/scripts.js
+++ tetris/js/scripts.js
@@ -4,26 +4,29 @@ $(function () {
 
     const TETRIS_FIELD_HEIGHT = 20;
     const TETRIS_FIELD_WIDTH = 10;
-    let tetrisFieldArray = [];
+    let tetrisFieldArray = [];//0 for an empty cell, 1 for an active tetromino cell, 2 for taken inactive call
     let activePositions = [[2, 4], [2, 5], [3, 4], [3, 5]];
     let turnTimer;
 
     prepareFieldArray();
     setKeyListener();
+    initialDebris();
     updateFieldArray(1);//display initial set up
-    test(); //TODO delete this later
     turnTimer = setInterval(changeTurn, 1000); //TODO use clearInterval(turnTimer) on game over
 
+    function generateFieldRow() {
+        let tetrisRowArray = [];
+        for (let j = 0; j < TETRIS_FIELD_WIDTH; j++) {//TODO make a blank row as a global variable
+            tetrisRowArray.push(0);
+        }
+        return tetrisRowArray;
+    }
     /**
      * Prep the array that will represent the playing field
      */
     function prepareFieldArray() {
         for (let i = 0; i < TETRIS_FIELD_HEIGHT + 4; i++) {// +4 is for the hidden spawning area
-            let tetrisRowArray = [];
-            for (let j = 0; j < TETRIS_FIELD_WIDTH; j++) {
-                tetrisRowArray.push(0);
-            }
-            tetrisFieldArray.push(tetrisRowArray);
+            tetrisFieldArray.push(generateFieldRow());
         }
     }
 
@@ -50,6 +53,28 @@ $(function () {
             tetrisFieldArray[activePositions[positionNo][0]][activePositions[positionNo][1]] = number;
         }
         displayFieldArray();
+    }
+
+    /**
+     * Specify initial taken cells
+     */
+    function initialDebris() {//TODO rework this to take an array
+        tetrisFieldArray[23][0] = 2;
+        tetrisFieldArray[22][0] = 2;
+        tetrisFieldArray[23][1] = 2;
+        tetrisFieldArray[22][1] = 2;
+        tetrisFieldArray[23][2] = 2;
+        tetrisFieldArray[22][2] = 2;
+        tetrisFieldArray[23][3] = 2;
+        tetrisFieldArray[22][3] = 2;
+        tetrisFieldArray[23][6] = 2;
+        tetrisFieldArray[22][6] = 2;
+        tetrisFieldArray[23][7] = 2;
+        tetrisFieldArray[22][7] = 2;
+        tetrisFieldArray[23][8] = 2;
+        tetrisFieldArray[22][8] = 2;
+        tetrisFieldArray[23][9] = 2;
+        tetrisFieldArray[22][9] = 2;
     }
 
     /**
@@ -105,8 +130,8 @@ $(function () {
             let newColPosition = activePositions[positionNo][1] + colShift;
             if (newRowPosition < 0 || newRowPosition >= TETRIS_FIELD_HEIGHT + 4 || newColPosition < 0 || newColPosition >= TETRIS_FIELD_WIDTH)
                 moveIsValid = false; //tetris field edge collision
-            if (tetrisFieldArray[newRowPosition][newColPosition] === 2)
-                moveIsValid = false //taken cell collision
+            else  if (tetrisFieldArray[newRowPosition][newColPosition] === 2)
+                moveIsValid = false; //taken cell collision
         }
         return moveIsValid;
     }
@@ -122,8 +147,14 @@ $(function () {
             if (tetrisFieldArray[rowNo][cellNo] === 2)
                 takenCells ++;
         }
-        console.log(takenCells);
         return takenCells;
+    }
+
+    function checkAndClearRow(rowNo) {
+        if (checkRow(rowNo) === TETRIS_FIELD_WIDTH) {
+            tetrisFieldArray.splice(rowNo, 1);//delete the filled row
+            tetrisFieldArray.splice(0, 0, generateFieldRow());//add a new row at the top
+        }
     }
 
     /**
@@ -139,15 +170,14 @@ $(function () {
                 return;
             }
             updateFieldArray(2);//change active tetromino to inactive cells
+            for (let rowNo in tetrisFieldArray) {
+                checkAndClearRow(rowNo);
+            }
             activePositions = [[2, 4], [2, 5], [3, 4], [3, 5]];//spawn a new tetromino TODO make it a random tetromino
             changeTurn();//do not skip a turn when spawning a new tetromino
         }
     }
 
-    function test() {//TODO delete this later
-        tetrisFieldArray[10][4] = 2;
-        tetrisFieldArray[10][5] = 2;
-        displayFieldArray()
-    }
+
 
 });
